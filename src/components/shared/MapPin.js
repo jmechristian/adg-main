574695e5f2e1621dc018da7adbd870372210fb12
jmechristian <jmechristian@gmail.com>
2025-06-18 19:19:21 -0400

    sdsdsd

--- src/components/shared/MapPin.js
+++ src/components/shared/MapPin.js
@@ -7,52 +7,41 @@ export default function MapPin() {
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <defs>
-        {/* Pattern for the pin texture */}
-        <pattern
-          id='pinTexture'
-          patternUnits='userSpaceOnUse'
-          width='24'
-          height='32'
-          patternContentUnits='objectBoundingBox'
-        >
-          <image
-            href='https://adgadmin170407-dev.s3.us-east-1.amazonaws.com/map-pin.png'
-            width='1'
-            height='1'
-            preserveAspectRatio='xMidYMid slice'
-          />
-        </pattern>
-      </defs>
-
-      {/* Shadow */}
-      <ellipse cx='12' cy='30' rx='8' ry='2' fill='rgba(0, 0, 0, 0.3)' />
-
-      {/* Pin with texture */}
-      <path
-        d='M12 0C5.373 0 0 5.373 0 12C0 18.627 12 32 12 32C12 32 24 18.627 24 12C24 5.373 18.627 0 12 0Z'
-        fill='url(#pinTexture)'
-      />
-
-      {/* Shadow filter */}
+      {/* Drop shadow filter */}
       <defs>
         <filter
-          id='shadow'
-          x='-4'
-          y='-4'
-          width='32'
-          height='40'
-          filterUnits='userSpaceOnUse'
+          id='dropShadow'
+          x='-20%'
+          y='-20%'
+          width='140%'
+          height='140%'
+          filterUnits='objectBoundingBox'
         >
-          <feGaussianBlur in='SourceAlpha' stdDeviation='1.5' />
-          <feOffset dy='1' />
-          <feComposite in2='SourceAlpha' operator='arithmetic' k2='-1' k3='1' />
-          <feColorMatrix
-            type='matrix'
-            values='0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.3 0'
-          />
+          <feGaussianBlur in='SourceAlpha' stdDeviation='2' />
+          <feOffset dx='0' dy='2' result='offset' />
+          <feFlood floodColor='rgba(0,0,0,0.3)' />
+          <feComposite in2='offset' operator='in' />
+          <feMerge>
+            <feMergeNode />
+            <feMergeNode in='SourceGraphic' />
+          </feMerge>
         </filter>
+
+        {/* Mask for creating the cutout */}
+        <mask id='pinMask'>
+          <rect width='24' height='32' fill='white' />
+          <circle cx='12' cy='12' r='4' fill='black' />
+        </mask>
       </defs>
+
+      {/* Pin with circular cutout and drop shadow */}
+      <g filter='url(#dropShadow)'>
+        <path
+          d='M12 0C5.373 0 0 5.373 0 12C0 18.627 12 32 12 32C12 32 24 18.627 24 12C24 5.373 18.627 0 12 0Z'
+          fill='#727272'
+          mask='url(#pinMask)'
+        />
+      </g>
     </svg>
   );
 }
