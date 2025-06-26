aaa766ea4175c7d16240bdc140fbd182a9fe9104
jmechristian <jmechristian@gmail.com>
2025-06-25 20:42:39 -0400

    sssss

--- src/components/HomeVisit.js
+++ src/components/HomeVisit.js
@@ -6,8 +6,8 @@ const HomeVisit = ({ studioText, studioLink, studioImage, id }) => {
   const router = useRouter();
   return (
     <div className='w-full max-w-6xl mx-auto pb-16 px-4 md:!px-0'>
-      <div className='grid grid-cols-12 gap-20 items-center'>
-        <div className='col-span-12 md:col-span-6 flex flex-col gap-10'>
+      <div className='grid  grid-cols-1 md:!grid-cols-12 gap-20 items-center'>
+        <div className='col-span-1 md:col-span-6 flex flex-col gap-10'>
           <div className='text-brand-gray text-2xl font-brand font-light'>
             <div
               dangerouslySetInnerHTML={{ __html: studioText }}
@@ -22,7 +22,7 @@ const HomeVisit = ({ studioText, studioLink, studioImage, id }) => {
             Visit Our Studio
           </div>
         </div>
-        <div className='col-span-12 md:col-span-6 w-full h-full'>
+        <div className='col-span-1 md:col-span-6 w-full h-full'>
           <div
             className='w-full h-full bg-cover bg-center'
             style={{
