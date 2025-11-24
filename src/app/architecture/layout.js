d7fd08dc3553aebe55a4c4e998b184ec08390632
jmechristian <jmechristian@gmail.com>
2025-11-24 08:24:35 -0500

    titles

---
+++ src/app/architecture/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: 'Architecture | Akseizer Design Group',
+  description:
+    'Learn about Akseizer Design Group, a visionary studio of interior designers, architects, brand strategists, and builders.',
+};
+
+export default function StudioLayout({ children }) {
+  return children;
+}
